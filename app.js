6c341e085e9ae06291908946d86cb50521325ab7
Jaroslav Tomcala <72067382+jtomcala@users.noreply.github.com>
2024-12-11 14:19:03 +0000

    webpage

--- app.js
+++ app.js
@@ -27,7 +27,47 @@ function logWorkbookInformation() {
     index = element.index;
     console.log(`The worksheet with index ${index} is ${element.name}`);
   });
+
+  saleMap = listSheets.find((ws) => ws.name == "SaleMap");
+  totalSales = listSheets.find((ws) => ws.name == "Total Sales");
+  salesByProduct = listSheets.find((ws) => ws.name == "SalesbyProduct");
+  salesBySegment = listSheets.find((ws) => ws.name == "SalesbySegment");
 }
+
+const oregonWashingtonButton = document.getElementById("oregon_and_washington");
+const clearFilterButton = document.getElementById("clear_filter");
+const undoButton = document.getElementById("undo");
+
+function oregonWashFunction() {
+  // Log what is pressed
+  console.log(oregonWashingtonButton.value);
+
+  saleMap.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+  totalSales.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+  salesByProduct.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+  salesBySegment.applyFilterAsync("State", ["Washington", "Oregon"], "replace");
+}
+
+function clearStateFilter() {
+  // Log what is pressed
+  console.log(clearFilterButton.value);
+
+  saleMap.clearFilterAsync("State");
+  totalSales.clearFilterAsync("State");
+  salesByProduct.clearFilterAsync("State");
+  salesBySegment.clearFilterAsync("State");
+}
+
+function undo() {
+  //Log what is pressed
+  console.log(undoButton.value);
+
+  viz.undoAsync();
+}
+
+oregonWashingtonButton.addEventListener("click", oregonWashFunction);
+clearFilterButton.addEventListener("click", clearStateFilter);
+undoButton.addEventListener("click", undo);
 
 // run the function when viz becomes interactive.
 viz.addEventListener("firstinteractive", logWorkbookInformation);
